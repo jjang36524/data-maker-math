73f57f55a4dab06d6ea8e1c159e2747305b670cb
jjang36524 <jjang36524@naver.com>
2023-07-08 21:23:54 +0900

    a

--- Project31.cpp
+++ Project31.cpp
@@ -14,22 +14,24 @@ int main()
 	cout << "샘플링 주파수를 입력하세요 ";
 	int freq;
 	cin >> freq;
-	cout << "최저 주파수 : " << freq / (double)len;
+	cout << "최저 주파수 : " << freq / (double)len<<'\n';
 	while (1)
 	{
-		cout << "사인함수의 주파수를 입력하세요 : (최저 주파수 배수)";
+		cout << "사인함수의 주파수를 입력하세요 (최저 주파수를 곱해서 들어갑니다) :";
 		int a;
 		cin >> a;
-		cout << "사인함수의 진폭을 입력하세요 : ";
+		cout << "사인함수의 진폭을 입력하세요 : (0이면 종료)";
 		double b;
 		cin >> b;
+		if (b == 0)
+			break;
 		cout << "사인함수의 시작 위상을 입력하세요 : (0~2pi)";
 		double c;
 		cin >> c;
 		int i;
 		for (i = 0; i < len; i++)
 		{
-			val[i] += b * sin(i * PI * 2 * len / a+c);
+			val[i] += b * sin(i * PI * 2 * a/len+c);
 		}
 	}
 	freopen(arr, "w", stdout);
